fc3ccbcfb3810ba769ef3dca7f309da019a27c66
Shawn Doughnut <shawndou@an-h6.tash.21-school.uz>
2026-03-26 21:12:36 +0500

    707-283 soru cozumleri

---
+++ binarySearch/283_mover_zeroes.js
@@ -0,0 +1,18 @@
+var moveZeroes = function (nums) {
+      let countZero = 0;
+
+      for (let i = 0; i < nums.length; i++) {
+            if (nums[i] === 0) {
+                  nums.splice(i, 1); 
+                  countZero++;
+                  i--; 
+            }
+      }
+
+      while (countZero > 0) {
+            nums.push(0);
+            countZero--;
+      }
+
+      return nums;
+};
