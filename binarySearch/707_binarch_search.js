964db99420bd2dba36f5bdbe62e095cb4bb45b5c
Shawn Doughnut <shawndou@an-h6.tash.21-school.uz>
2026-03-26 21:09:06 +0500

    first push

---
+++ binarySearch/707_binarch_search.js
@@ -0,0 +1,25 @@
+let nums = [-1, 0, 3, 5, 9, 12];
+let target = 9;
+
+var search = function (nums, target) {
+      let start = 0;
+      let end = nums.length;
+
+      while (start < end) {
+            let middle = Math.floor((start + end) / 2);
+
+            if (nums[middle] === target) {
+                  return middle;
+            }
+
+            if (target < nums[middle]) {
+                  end = middle;        
+            } else {
+                  start = middle + 1;
+            }
+      }
+
+      return -1;
+};
+
+console.log(search(nums, target));
